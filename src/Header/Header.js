01d570bf238d778b10c5cc1e3d713097573abf10
Heesu Kang <hskang135@gmail.com>
2019-11-25 17:36:49 -0500

    some css

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,17 +7,17 @@ export default class Header extends Component {
   render() {
     return(
       <div className="header">
-          <h1 className="homepage">
-            <Link to='/'>
-              Home
-            </Link>
-          </h1>
+        <h1 className="homepage">
+          <Link to='/'>
+            Home
+          </Link>
+        </h1>
 
-          <h1 className="myrecipes">
-            <Link to='/myrecipes'> 
-              My Recipes 
-            </Link>   
-          </h1>
+        <h1 className="myrecipes">
+          <Link to='/myrecipes'> 
+            My Recipes 
+          </Link>   
+        </h1>
       </div>  
     )
   }
