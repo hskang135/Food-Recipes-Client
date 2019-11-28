d0edd6fce1bf964b4f72eb962cdd29548de2ff3c
Heesu Kang <hskang135@gmail.com>
2019-11-28 10:05:40 -0500

    add img

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://pure-forest-17389.herokuapp.com/api',
+  API_ENDPOINT: 'https://whispering-dawn-16121.herokuapp.com/api',
   API_KEY: process.env.REACT_APP_API_KEY
 };
 
